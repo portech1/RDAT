a62d7235bc897fe3f353b5b5513312588895a0ef
Chris Porter <chporter@southeastrans.com>
2020-04-23 20:11:15 -0400

    Create and View Batches

--- RDAT/Controllers/ReportsController.cs
+++ RDAT/Controllers/ReportsController.cs
@@ -74,7 +74,7 @@ namespace RDAT.Controllers
                     // First get rid of old entries
                     List<TempTestingLog> existingEntries = context.TempTestingLogs.ToList();
                     context.TempTestingLogs.RemoveRange(existingEntries);
-                    
+
                     activeDrivers = context.Drivers.Where(d => d.EnrollmentDate > d.TerminationDate).Count();
                     int numDrug = (int)(_percentDrug * activeDrivers);
                     int numAlcohol = (int)(_percentAlcohol * activeDrivers);
@@ -90,6 +90,7 @@ namespace RDAT.Controllers
                         TempTestingLog _tempLog = new TempTestingLog();
                         _tempLog.Test_Type = "Drug";
                         _tempLog.Driver_Id = d.Id;
+                        _tempLog.Driver_Name = d.DriverName;
                         _tempLog.CreatedDate = DateTime.Now;
                         _tempLog.ModifiedDate = DateTime.Now;
                         context.TempTestingLogs.Add(_tempLog);
@@ -101,6 +102,7 @@ namespace RDAT.Controllers
                         TempTestingLog _tempLog = new TempTestingLog();
                         _tempLog.Test_Type = "Alcohol";
                         _tempLog.Driver_Id = d.Id;
+                        _tempLog.Driver_Name = d.DriverName;
                         _tempLog.CreatedDate = DateTime.Now;
                         _tempLog.ModifiedDate = DateTime.Now;
                         context.TempTestingLogs.Add(_tempLog);
@@ -119,7 +121,7 @@ namespace RDAT.Controllers
 
 
             }
-            
+
 
             _model.tempTestingLogs = _tempTestingLog;
             _model.batchRequest = new CreateBatch();
@@ -146,19 +148,107 @@ namespace RDAT.Controllers
 
         public IActionResult UpdateBatch(CreateBatchViewModel model)
         {
-            if (ModelState.IsValid)
+            // Create Batch
+
+            Batch _batch = new Batch();
+            var _tempTestingLog = new List<TempTestingLog>();
+
+            using (RDATContext context = new RDATContext())
             {
-                //_context.Add(createBatch);
-                //await _context.SaveChangesAsync();
-                //return RedirectToAction(nameof(Index));
+                // get list of existing test logs
+                _tempTestingLog = context.TempTestingLogs.ToList();
+                // get active drivers
+                int activeDrivers = context.Drivers.Where(d => d.EnrollmentDate > d.TerminationDate).Count();
+                int numDrug = context.TempTestingLogs.Where(t => t.Test_Type == "Drug").Count();
+                int numAlcohol = context.TempTestingLogs.Where(t => t.Test_Type == "Alcohol").Count();
+                float _percentDrug = ((float)numDrug / activeDrivers) * 100;
+                float _percentAlcohol = ((float)numAlcohol / activeDrivers) * 100;
+                ViewBag.percentDrug = _percentDrug.ToString("##");
+                ViewBag.percentAlcohol = _percentAlcohol.ToString("##");
+                ViewBag.totalDrivers = numDrug + numAlcohol;
+                ViewBag.activeDrivers = activeDrivers;
+
+                // Create the Batch
+                _batch.Created = DateTime.Now;
+                _batch.Alcohol_Percentage = (int)_percentAlcohol;
+                _batch.Drug_Percentage = (int)_percentDrug;
+                _batch.Alcohol_Tests = numAlcohol;
+                _batch.Drug_Tests = numDrug;
+                _batch.Eligible_Drivers = activeDrivers;
+
+                context.Batches.Add(_batch);
+                context.SaveChanges();
+
+                // Get New Batch ID
+                int batchID = _batch.Id;
+                // Build Batch
+                BuildBatch(batchID);
+                ClearTempBatch();
             }
-            var me = model.batchRequest.ActivePool;
-            return View(model);
+
+            return RedirectToAction("ViewBatch", "Reports");
+        }
+
+        private bool BuildBatch(int id)
+        {
+            using (RDATContext context = new RDATContext())
+            {
+                List<TempTestingLog> _logs = context.TempTestingLogs.ToList();
+
+                foreach (TempTestingLog log in _logs)
+                {
+                    TestingLog _testingLog = new TestingLog();
+                    _testingLog.Test_Type = log.Test_Type;
+                    _testingLog.Driver_Id = log.Driver_Id;
+                    _testingLog.Created = log.CreatedDate;
+                    _testingLog.Modified = log.ModifiedDate;
+                    _testingLog.Batch_Id = id;
+
+                    context.TestingLogs.Add(_testingLog);
+                                                         
+                }
+
+                context.SaveChanges();
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool ClearTempBatch()
+        {
+
+            try
+            {
+                using (RDATContext context = new RDATContext())
+                {
+                    List<TempTestingLog> existingEntries = context.TempTestingLogs.ToList();
+                    context.TempTestingLogs.RemoveRange(existingEntries);
+                    context.SaveChanges();
+
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
         }
 
         public IActionResult Company()
         {
             return View();
+        }
+        public IActionResult ViewBatch()
+        {
+            ViewBatchViewModel _model = new ViewBatchViewModel();
+            using (RDATContext context = new RDATContext())
+            {
+                _model.Batches = context.Batches.ToList();
+            }
+
+            return View(_model);
         }
     }
 }
